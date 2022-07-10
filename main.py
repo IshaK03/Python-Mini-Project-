79c12b1f5043e520d1efd99f31e3eb2b150985a7
IshaK03 <101555713+IshaK03@users.noreply.github.com>
2022-07-11 00:36:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random # For generating random numbers
 import sys # We will use sys.exit to exit the program
 import pygame
 from pygame.locals import * # Basic pygame imports
+import time
 
 pygame.init()
 pygame.font.init()
